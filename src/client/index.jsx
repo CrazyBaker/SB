fcbbbe3c27d8bb6e95541d9bec85fdee51ed3f93
CrazyBacker <zigunovchrome@gmail.com>
2022-11-20 15:00:34 +0300

    add hmr server

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,6 +5,5 @@ import { Header } from "../shared/Header";
 window.addEventListener("load", () => {
   //ReactDOM.hydrate(<Header />, document.getElementById("react_root"));
   //LS 2-2
-
   const root = ReactDOM.hydrateRoot(document.getElementById("react_root"), <Header />);
 });
